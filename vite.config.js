a1fd292aa4e8fb5fc1419be11d2bcd052a0a673e
Víctor Tamayo <victortamayo509@gmail.com>
2022-08-01 19:27:15 -0500

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://VictorTamayoBriones.github.io/gh-pages/',
+  base: 'https://victortamayobriones.github.io/gh-actions/',
   plugins: [react()],
 })
